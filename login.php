605e00303f770aa7caf43c37a333bfa5d2ae38b9
angga88888 <fingerstyle88888@gmail.com>
2018-12-09 16:21:47 +0800

    push login update

--- login.php
+++ login.php
@@ -1,147 +1,82 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-body {font-family: Arial, Helvetica, sans-serif;}
-.container {
-    width: 500px;
-    margin: 0 auto;
-    border-radius: 2px;
-    padding: 25px;
-    box-shadow: 0px 0px 3px 0px #b9b6b6;
-    margin-top: 40px;
-    margin-top: 15%;
-}
-.alert {
-    padding: 20px;
-    text-align: center;
-    /* width: fit-content; */
-    border-radius: 5px;
-    background: #dc3030f0;
-    color: white;
-    margin: 0 auto;
-    display: none;
-}
-.success {
-    padding: 20px;
-    text-align: center;
-    /* width: fit-content; */
-    border-radius: 5px;
-    background: #39ab3d;
-    color: white;
-    margin: 0 auto;
-    display: none;
-    font-weight: bold;
-}
-/* Full-width input fields */
-input[type=text], input[type=password] {
-    width: 100%;
-    padding: 12px 20px;
-    margin: 8px 0;
-    display: inline-block;
-    border: 1px solid #ccc;
-    box-sizing: border-box;
+<?php
+
+include('koneksi.php');
+
+session_start();
+
+$message = '';
+
+//membuat user tetap masuk saat mengakses login bila belum logout
+if(isset($_SESSION['id_user']))
+{
+ header('location:welcome.php');
 }
 
-/* Set a style for all buttons */
-button {
-    background-color: #4CAF50;
-    color: white;
-    padding: 14px 20px;
-    margin: 8px 0;
-    border: none;
-    cursor: pointer;
-    width: 100%;
+if(isset($_POST["login"]))
+{
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  $sql = "SELECT * FROM user WHERE username = '$username'";
+    $result = mysqli_query($koneksi,$sql) or die(mysqli_error($koneksi));      
+    $count = mysqli_num_rows($result);
+    //mengambil data untuk satu baris pada database
+    $data = $result->fetch_array(MYSQLI_NUM);
+  if($count > 0)
+ {
+  #mengecek password
+    if($_POST["password"] == $data[3])
+    {
+        $_SESSION['id_user'] = $data[0];
+        $_SESSION['username'] = $username;
+        header("location:welcome.php");
+    }
+    else
+    {
+      $message = "<label>Password Salah</label>";    
+    }
+}
+ else
+ {
+  $message = "<label>Username Salah</label>";
+ }
 }
 
-button:hover {
-    opacity: 0.8;
-}
+?>
 
-/* Extra styles for the cancel button */
-.cancelbtn {
-    width: auto;
-    padding: 10px 18px;
-    background-color: #f44336;
-}
-
-/* Center the image and position the close button */
-.imgcontainer {
-    text-align: center;
-    margin: 24px 0 12px 0;
-    position: relative;
-}
-
-
-span.psw {
-    float: right;
-    padding-top: 16px;
-}
-</style>
-</head>
-<body>
-
-    <div class="alert"></div>
-    <div class="success">Login Berhasil</div>
-    <!-- <h2 class="imgcontainer">Login Form AJAX</h2>   -->
-    <form id="form-login" action="" method="post">
+<html>  
+    <head>  
+        <title>Aplikasi Komentar</title>  
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    </head>  
+    <body>  
         <div class="container">
-          <label for="uname"><b>Username</b></label>
-          <input type="text" placeholder="Enter Username" name="username">
-          <label for="psw"><b>Password</b></label>
-          <input type="password" placeholder="Enter Password" name="password">
-          <button type="submit">Login</button>
-          
-        </div>
-       
-    </form>
-
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script type="text/javascript">
-      $(document).ready(function(){
-        $('#form-login').submit(function(e){
-          //get data login
-          dataLogin = $(this).serializeArray();
-          console.log(dataLogin);
-          //cek validasi kosong
-          if($('input[name="username"]').val() == "")
-          {
-
-            $('.alert').html('<b>Kolom Username</b> tidak boleh kosong').fadeIn("fast").fadeOut(3000);;
-            $('input[name="username"]').focus();
-            return false;
-          }
-          else if($('input[name="password"]').val() == "")
-          {
-            $('.alert').html('<b>Kolom Password</b> tidak boleh kosong').fadeIn("fast").fadeOut(3000);;
-            $('input[name="password"]').focus();
-            return false;
-          }
-          //ajax
-          $.ajax({
-            type : 'POST',
-            data : dataLogin,
-            url : 'proses.php',
-            success : function(data){
-              if(data == "success")
-              {
-                $('.success').fadeIn("fast").fadeOut(5000);
-                setTimeout(function(){
-                  window.location.href = "welcome.php";
-                },1234);
-              }
-              else if(data == "failed")
-              {
-                $('.alert').html('<b>Kombinasi Password & Username Salah</b> ').fadeIn("fast").fadeOut(3000);
-              }
-
-            }
-          });
-
-          e.preventDefault();
-        });
-      });
-    </script>
-</body>
+   <br />
+   
+   <h3 align="center">Aplikasi Komentar</a></h3><br />
+   <br />
+   <div class="panel panel-default">
+      <div class="panel-heading">Login Aplikasi Komentar</div>
+    <div class="panel-body">
+     <form method="post">
+      <p class="text-danger"><?php echo $message; ?></p>
+      <div class="form-group">
+       <label>Masukkan Username</label>
+       <input type="text" name="username" class="form-control" required />
+      </div>
+      <div class="form-group">
+       <label>Masukkan Password</label>
+       <input type="password" name="password" class="form-control" required />
+      </div>
+      <div class="form-group">
+       <input type="submit" name="login" class="btn btn-info" value="Login" />
+      </div>
+     </form>
+    </div>
+   </div>
+  </div>
+    </body>  
 </html>
--- welcome.php
+++ welcome.php
@@ -1,27 +1,36 @@
 <?php
-    include("koneksi.php");
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Selamat Datang</title>
-	<style type="text/css">
-		.welcome {
-    padding: 100px;
-    font-size: 50px;
-    text-align: center;
-    width: fit-content;
-    margin: 0 auto;
-    background: whitesmoke;
-    border-radius: 5px;
-    box-shadow: 0px 0px 5px 0px #ccc0c0;
+
+include('koneksi.php');
+
+session_start();
+
+//membuat user tetap masuk sebelum di logout
+if(!isset($_SESSION['id_user']))
+{
+ header('location:login.php');
 }
-	</style>
-</head>
-<body>
-	<div class="welcome">
-		Selamat Datang
-	</div>
-    <h3><a href="login.php">Log Out</a></h3>
-</body>
-</html>
+?>
+
+<html>  
+    <head>  
+        <title>Aplikasi Komentar</title>  
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    </head>  
+    <body>  
+        <div class="container">
+   <br />
+   
+   <h3 align="center">Aplikasi Komentar</a></h3><br />
+   <br />
+   
+   <div class="table-responsive">
+    <h4 align="center">Online User</h4>
+    <p align="right">Hi - <?php echo $_SESSION['username']; ?> - <a href="logout.php">Logout</a></p>
+    
+   </div>
+  </div>
+    </body>  
+</html>  
